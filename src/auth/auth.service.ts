366418f55e30722469bd900cf33511ecd5e787e8
Leandro Arturi <lea.arturi@gmail.com>
2023-02-08 13:27:42 -0300

    Generar JWT en Login

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,25 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+
 import { AuthResponse } from './types/auth-response.type';
 import { UsersService } from '../users/users.service';
 import { LoginInput, SignupInput } from './dto/inputs';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  private getJwtToken(userId: string) {
+    return this.jwtService.sign({ id: userId });
+  }
 
   async signup(signupInput: SignupInput): Promise<AuthResponse> {
     const user = await this.usersService.create(signupInput);
-    const token = 'myTokenTemp';
+    const token = this.getJwtToken(user.id);
 
     return {
       token,
@@ -20,7 +29,7 @@ export class AuthService {
 
   async login({ email, password }: LoginInput): Promise<AuthResponse> {
     const user = await this.usersService.findOneByEmail(email);
-    const token = 'myTokenTemp';
+    const token = this.getJwtToken(user.id);
 
     if (!bcrypt.compareSync(password, user.password)) {
       throw new BadRequestException('Email / Password do not match');
